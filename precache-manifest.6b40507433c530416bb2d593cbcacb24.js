ed29655c9d2641b253d837ed8047b09fb077ff75
System Administrator <root@Pratikshyas-MacBook-Pro.local>
2020-02-23 22:05:01 -0600

    Updates

---
+++ precache-manifest.6b40507433c530416bb2d593cbcacb24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cece1918bfd1fc74eef45ce9ed1b7f4d",
+    "url": "/pookbook-client/index.html"
+  },
+  {
+    "revision": "058f4c2849f2f7d92ce7",
+    "url": "/pookbook-client/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "052fdee062c03f1e29c8",
+    "url": "/pookbook-client/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "058f4c2849f2f7d92ce7",
+    "url": "/pookbook-client/static/js/2.16468cd4.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/pookbook-client/static/js/2.16468cd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "052fdee062c03f1e29c8",
+    "url": "/pookbook-client/static/js/main.58390a3c.chunk.js"
+  },
+  {
+    "revision": "6604f6ffb19773be5033",
+    "url": "/pookbook-client/static/js/runtime-main.93847314.js"
+  }
+]);
